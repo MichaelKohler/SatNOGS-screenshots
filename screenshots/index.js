fbd5c5e6fdec9671ba068f090452e526e5fada5b
Michael Kohler <me@michaelkohler.info>
2020-09-12 23:30:53 +0200

    Create zip from all screenshots

--- screenshots/index.js
+++ screenshots/index.js
@@ -1,25 +1,50 @@
 const fs = require('fs');
+const archiver = require('archiver');
 const puppeteer = require('puppeteer');
 
+const SCREENSHOTS_PATH = '/tmp/satnogs-images';
+const ZIP_OUTPUT_FILE = '/tmp/screenshots.zip';
+
 module.exports = async function(context, req) {
-    const fromIndex = parseInt(req.query.from, 10);
-    const toIndex = parseInt(req.query.to, 10);
-    const browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: null,
-      args: ['--start-maximized']
-    });
-    fs.mkdirSync('/tmp/satnogs-images');
+  const fromIndex = parseInt(req.query.from, 10);
+  const toIndex = parseInt(req.query.to, 10);
 
-    const screenshotBuffer = await screenshot(browser, 100);
-    await browser.close();
-
+  if (typeof fromIndex !== 'number' || typeof toIndex !== 'number') {
     context.res = {
-      body: screenshotBuffer,
-      headers: {
-        'content-type': 'image/png',
-      },
+      body: 'to and from parameters required and needs to be a number!',
     };
+
+    return;
+  }
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    args: ['--start-maximized'],
+  });
+
+  try {
+    fs.mkdirSync(SCREENSHOTS_PATH);
+  } catch (error) {
+    // ignore..
+  }
+
+  for (let index = fromIndex; index <= toIndex; index++) {
+    console.log(`Creating screenshot for ${index}`);
+    await screenshot(browser, index);
+  }
+
+  await browser.close();
+
+  const buffer = await createZip();
+
+  context.res = {
+    body: buffer,
+    headers: {
+      'content-type': 'application/zip',
+      'content-disposition': 'attachment; filename="screenshots.zip"',
+    },
+  };
 };
 
 async function screenshot(browser, index) {
@@ -31,15 +56,56 @@ async function screenshot(browser, index) {
     height: 800,
     deviceScaleFactor: 2,
   });
-  const screenshotBuffer = await page.screenshot({
-    path: `/tmp/satnogs-images/${getPaddedNumber(index)}.jpg`,
+  await page.screenshot({
+    path: `${SCREENSHOTS_PATH}/${getPaddedNumber(index)}.jpg`,
     type: 'jpeg',
   });
   await page.close();
-
-  return screenshotBuffer;
 }
 
 function getPaddedNumber(index) {
   return ('0000' + index).substr(-4,4);
+}
+
+function createZip() {
+  console.log('Creating zip archive..');
+
+  const output = fs.createWriteStream(ZIP_OUTPUT_FILE);
+  const archive = archiver('zip', {
+    zlib: { level: 9 },
+  });
+
+  output.on('end', () => {
+    console.log('Data has been drained');
+  });
+
+  archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      console.log(err);
+      return;
+    }
+
+    throw err;
+  });
+
+  archive.on('error', (err) => {
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.directory(SCREENSHOTS_PATH, false);
+  archive.finalize();
+
+  return new Promise((resolve) => {
+    output.on('close', () => {
+      console.log(archive.pointer() + ' total bytes');
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+
+      const stats = fs.statSync(ZIP_OUTPUT_FILE);
+      console.log(`Zip Archive: ${stats.size} bytes`);
+      const rawFile = fs.readFileSync(ZIP_OUTPUT_FILE);
+      const buffer = Buffer.from(rawFile, 'base64');
+      resolve(buffer);
+    });
+  });
 }
